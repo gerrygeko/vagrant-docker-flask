7dcc7a3ad33e4de299421422a15266d9f08c3183
gerrygeko <lamorte.gerardo@gmail.com>
2019-01-30 11:21:09 +0100

    Add the user Vagrant to the Docker group

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,6 @@ sudo apt-get install -y python3-pip python3-setuptools &&
 sudo apt-get autoclean -y &&
 sudo apt-get clean -y &&
 pip3 install wheel
+
+sudo usermod -aG docker vagrant
 echo "----------------------END OF THE PROVISIONING----------------------"
