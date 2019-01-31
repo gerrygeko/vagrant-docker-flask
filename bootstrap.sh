20f7218fe28ff1c9bfde100fb038c380b6c343af
gerrygeko <lamorte.gerardo@gmail.com>
2019-01-31 11:29:01 +0100

    Refactoring

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-echo "----------------------PROVISIONING OF THE BOX----------------------"
+echo "-------------------------------------------------------------------"
+echo "|                     PROVISIONING OF THE BOX                     |"
+echo "-------------------------------------------------------------------"
 sudo apt-get install -y python3-pip python3-setuptools &&
 sudo apt-get autoclean -y &&
 sudo apt-get clean -y &&
 pip3 install wheel
-
 sudo usermod -aG docker vagrant
-echo "----------------------END OF THE PROVISIONING----------------------"
+echo "-------------------------------------------------------------------"
+echo "|                     END OF THE PROVISIONING                     |"
+echo "-------------------------------------------------------------------"
