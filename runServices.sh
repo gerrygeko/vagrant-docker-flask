bcc2c8755106ea4f6ca1d970147d072201835c82
gerrygeko <lamorte.gerardo@gmail.com>
2019-03-12 18:21:58 +0100

    More tweaking to scripts, added name of running services

--- runServices.sh
+++ runServices.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker run -d -p 5000:80 jetty-webapp
-docker run -d -p 80:80 py-webserver
+docker run -d -p 80:80 --name webapp jetty-webapp
+docker run -d -p 5000:80 --name  webserver py-webserver
